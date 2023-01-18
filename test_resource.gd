d3226ee8a29d75876a249846373348fe552e6b01
LemonadeFlashbang <81896582+LemonadeFlashbang@users.noreply.github.com>
2023-01-18 15:55:13 -0800

    Complete Test Case

---
+++ test_resource.gd
@@ -0,0 +1,13 @@
+extends Resource
+class_name Example
+
+export var img_1 : Texture
+export var img_2 : Texture
+export var img_3 : Texture
+export var img_4 : Texture
+export var img_5 : Texture
+export var img_6 : Texture
+export var img_7 : Texture
+export var img_8 : Texture
+export var img_9 : Texture
+export var img_10 : Texture
